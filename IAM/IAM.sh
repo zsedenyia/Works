9b58d8d2982520487510e7bf73d36da27b1c19ad
zsedenyia <attilazsedenyi@me.com>
2025-11-13 14:12:07 +0100

    update

---
+++ IAM/IAM.sh
@@ -0,0 +1,26 @@
+# Add a new user to Azure AD
+
+az ad user create --display-name myuser --password password --user-principal-name myuser@contoso.com
+
+# Create a Resource Group
+
+az group create --name MyResourceGroup --location NorthEurope
+
+# Create a Blob Storage Account
+az storage account create --name mystorageaccount --resource-group MyResourceGroup --location NorthEurope --sku Standard_LRS
+# Create a Container in the Storage Account
+az storage container create --name mycontainer --account-name mystorageaccount
+# Upload a file to the Container
+az storage blob upload --container-name mycontainer --file ./myfile.txt --name myfile.txt --account-name mystorageaccount
+# List Blobs in the Container
+az storage blob list --container-name mycontainer --account-name mystorageaccount --output table
+# Download a Blob from the Container
+az storage blob download --container-name mycontainer --name myfile.txt --file ./downloaded_myfile.txt --account-name mystorageaccount
+# Delete a Blob from the Container
+az storage blob delete --container-name mycontainer --name myfile.txt --account-name mystorageaccount
+# Delete the Container
+az storage container delete --name mycontainer --account-name mystorageaccount
+# Delete the Storage Account
+az storage account delete --name mystorageaccount --resource-group MyResourceGroup --yes
+# Delete the Resource Group
+az group delete --name MyResourceGroup --yes --no-wait
--- IAM/adduser.sh
+++
@@ -1,1 +0,0 @@
-az ad user create --display-name "Alexandraa Zettervall" --user-principal-name alexandra@yourdomain.onmicrosoft.com --password "Jamie2019" --force-change-password-next-login true
