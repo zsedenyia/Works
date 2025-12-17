2e73af347685d1cffa42858d7c8bca772d944abb
zsedenyia <attilazsedenyi@me.com>
2025-12-17 14:53:24 +0100

    change

--- IAM Kurs/Dag 1/HelloWorld/HelloWorld/Program.cs
+++ IAM Kurs/Dag 1/HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string name = "Attila";
+
+Console.WriteLine($"Hello World, Its me {name}!");
--- IAM Kurs/Dag 1/HelloWorld/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ IAM Kurs/Dag 1/HelloWorld/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc6b4ff41271a7dd65ce42f6bf0a5161e999635")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa94a27bb608c89790c804b3f6022cbc2564e29b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
