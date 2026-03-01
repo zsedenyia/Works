1bc9fbafa47e475c0b842546cb5fbd9e1a74f44d
zsedenyia <attilazsedenyi@me.com>
2026-03-01 21:33:41 +0100

    u

---
+++ IAM/demoapp/demoapp/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace demoapp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
