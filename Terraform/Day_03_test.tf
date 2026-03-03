0d905aa76fcdd3100f9201f15bcdc8bd679daafa
zsedenyia <attilazsedenyi@me.com>
2026-03-03 11:47:40 +0100

    u

--- Terraform/Day_03_test.tf
+++ Terraform/Day_03_test.tf
@@ -1,5 +1,7 @@
 #  az login --tenant 1359854f-2e91-42f1-800f-38df9a47df38
 
+# Create a service principle with the following command and use the output to fill in the values below:
+#  MSYS_NO_PATHCONV=1 az ad sp create-for-rbac -n az-demo --role Contributor --scopes /subscriptions/0c7b76e5-0b4f-4730-8f85-fb86f79473fc
 
 terraform {
   required_providers {
@@ -15,3 +17,4 @@ provider "azurerm" {
 
   }
 }
+
